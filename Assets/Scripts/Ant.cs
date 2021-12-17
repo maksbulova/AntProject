c19f791763f34e60724f2dc6daa8e08752978eea
maksbulova <maks.bulova@gmail.com>
2021-12-18 01:03:44 +0200

    Initial commit

---
+++ Assets/Scripts/Ant.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : MonoBehaviour
+{
+    private FeromoneMatrix feromoneMatrix;
+    [Range(0, 4)] public int angle;
+    public float feromoneAmount;
+
+    private const float baseChanse = 0.1f;
+
+    private enum BehaviorState{searching, carrying};
+    private BehaviorState behaviorState;
+    private Vector2Int currentDirection;
+
+
+    private void Start()
+    {
+        feromoneMatrix = GameObject.FindObjectOfType<FeromoneMatrix>();
+        behaviorState = BehaviorState.searching;
+        currentDirection = RandomDirection();
+    }
+
+    private void Update()
+    {
+        Act();
+    }
+
+    private Vector2Int RandomDirection()
+    {
+        int x,y;
+        do
+        {
+            x = Random.Range(-1, 2);
+            y = Random.Range(-1, 2);
+
+        } while (x == 0 && y == 0);
+
+        return new Vector2Int(x, y);
+    }
+
+    public void Move(Vector3 movePosition)
+    {
+        gameObject.transform.position = movePosition;   
+    }
+
+    public Vector2Int ChooseCell(int feromonType)
+    {
+        int amount = (angle + 1) * 2 - 1;
+        Vector2Int[] possibleCells = new Vector2Int[amount];
+        float[] possibleFeromones = new float[amount]; 
+        Vector2Int currentCellIndex = feromoneMatrix.GetCellIndex(transform.position);
+        Vector2Int cellPosition;
+
+
+        for (int i = -angle; i <= angle; i++)
+        {
+            float rotateAngle = i * Mathf.PI / 4;
+            int rotatedX = Mathf.RoundToInt(currentDirection.x * Mathf.Cos(rotateAngle) + currentDirection.y * Mathf.Sin(rotateAngle));
+            int rotadedY = Mathf.RoundToInt(currentDirection.x * Mathf.Sin(rotateAngle) + currentDirection.y * Mathf.Cos(rotateAngle));
+            Vector2Int newDirection = new Vector2Int(rotatedX, rotadedY);
+
+            cellPosition = currentCellIndex + newDirection;
+            possibleCells[i + angle] = cellPosition;
+            possibleFeromones[i + angle] = feromoneMatrix.GetSmell(cellPosition, feromonType);
+            // Debug.DrawLine(transform.position, transform.position + new Vector3(newDirection.x, newDirection.y) * 10, Color.yellow, 10);
+        }
+
+        // Debug.DrawLine(transform.position, transform.position + new Vector3(currentDirection.x, currentDirection.y) * 10, Color.red, 10);
+
+        float sumFeromone = 0;
+        float[] p = new float[possibleFeromones.Length];
+
+        for (int i = 0; i < possibleFeromones.Length; i++)
+        {
+            p[i] =  possibleFeromones[i] + baseChanse;
+            sumFeromone += possibleFeromones[i] + baseChanse;
+        }
+
+        for (int i = 0; i < p.Length; i++)
+        {
+            p[i] /= sumFeromone;
+        }
+
+        float rnd = Random.Range(0f, 1f);
+        int chosenIndex = 0;
+        for (int i = 0; i < p.Length; i++)
+        {
+            if (rnd < p[i])
+            {
+                chosenIndex = i;
+                break;
+            }
+        }
+
+        return possibleCells[chosenIndex];
+    }
+
+    private void Act()
+    {
+        Vector2Int cell = ChooseCell((int)behaviorState);
+        Vector3 movePos = feromoneMatrix.GetCellPosition(cell);
+        Move(movePos);
+
+        feromoneMatrix.SetSmell(transform.position, (1 - (int)behaviorState), feromoneAmount);
+    }
+
+}
---
+++ Assets/Scripts/FeromoneMatrix.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeromoneMatrix : MonoBehaviour
+{
+    public Transform groundContainer;
+    public float cellSize;
+    public int matrixSize;
+    public float evaporateAmount;
+
+    private float[,,] feromoneMatrix;
+    private GameObject[,] planeMatrix;
+
+    private void Start()
+    {
+        GenerateField();
+        ColorFeromones();
+    }
+
+    private void Update()
+    {
+        ColorFeromones();
+        Evaporate();
+    }
+
+    private void GenerateField()
+    {
+        feromoneMatrix = new float[matrixSize, matrixSize, 2];
+        planeMatrix = new GameObject[matrixSize, matrixSize];
+
+        float offset = cellSize * matrixSize / 2;
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                plane.transform.SetParent(groundContainer);
+                float x = (i + 0.5f) * cellSize;
+                float y = (j + 0.5f) * cellSize;
+                plane.transform.position = new Vector3(x - offset, y - offset);
+                plane.transform.Rotate(-90, 0, 0);
+                plane.transform.localScale *= 0.1f * cellSize;
+                planeMatrix[i, j] = plane;
+            }
+        }
+    }
+
+    public Vector3 GetCellPosition(Vector2Int cellIndex)
+    {
+        float x = (cellIndex.x + 0.5f) * cellSize;
+        float y = (cellIndex.y + 0.5f) * cellSize;
+        return new Vector3(x, y);
+    }
+
+    private void ColorFeromones()
+    {
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                float feromon = feromoneMatrix[i, j, 0];
+                Color color = Color.Lerp(Color.white, Color.red, feromon);
+                planeMatrix[i, j].GetComponent<Renderer>().material.color = color;
+            }
+        }
+    }
+
+    public Vector2Int GetCellIndex(Vector3 position)
+    {
+        float offset = matrixSize * cellSize / 2;
+        int i = Mathf.FloorToInt((position.x + offset) / cellSize);
+        int j = Mathf.FloorToInt((position.y + offset) / cellSize);
+        return new Vector2Int(i, j);
+    }
+
+    public float GetSmell(Vector2Int cellIndex, int feromoneType)
+    {
+        return feromoneMatrix[cellIndex.x, cellIndex.y, feromoneType];
+    }
+
+
+    public void SetSmell(Vector3 position, int feromoneType, float value)
+    {
+        Vector2Int cellIndex = GetCellIndex(position);
+
+        float clamped = Mathf.Clamp(feromoneMatrix[cellIndex.x, cellIndex.y, feromoneType] += value, 0, 1);
+        feromoneMatrix[cellIndex.x, cellIndex.y, feromoneType] = clamped;
+    }
+
+    private void Evaporate()
+    {
+        for (int i = 0; i < matrixSize; i++)
+        {
+            for (int j = 0; j < matrixSize; j++)
+            {
+                feromoneMatrix[i, j, 0] -= evaporateAmount;
+                feromoneMatrix[i, j, 1] -= evaporateAmount;
+            }
+        }
+    }
+}
+
